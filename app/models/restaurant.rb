690f23e3729f0b9641b27d7570c7ed43fed4eb82
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-02-24 16:13:31 +0100

    install faker, set routes and controllers with actions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,11 @@
+CATEGORIES = %w[chinese italian japanese french belgian]
+
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: {
+    in: CATEGORIES,
+    message: "%{value} should be a category among #{CATEGORIES}"
+  }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Cleaning DB"
+Restaurant.destroy_all
+puts "DB cleaned"
+
+puts "Starting seeding..."
+
+5.times do
+  resto = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  resto.save!
+end
+
+puts "Seeding ended"
